25513a0aa2b8fe76b2940df5ca975f2401d081ca
nim-ka <raumaank1@gmail.com>
2021-12-29 03:20:55 -0500

    tampermonkey maybe?

--- cat.sh
+++ cat.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cat tampermonkey.txt dll.js pt.js grid.js binheap.js graph.js proto.js utils.js > out.js
+cat tampermonkey.txt dll.js pt.js grid.js binheap.js graph.js proto.js utils.js > out.user.js
