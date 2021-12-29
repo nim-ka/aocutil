e7ecba88ef5847fa6e1a19047e59c52ece62b7c9
nim-ka <raumaank1@gmail.com>
2021-12-29 03:28:49 -0500

    aa

--- out.user.js
+++ out.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         aocutil
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
-// @match        http://*/*
+// @match        *
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
