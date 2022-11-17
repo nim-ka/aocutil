372a65cc51a297acf0fdd187c8d4f3610299c19e
nim from KA <35317534+nim-ka@users.noreply.github.com>
2022-11-17 16:00:25 -0500

    Update 6.js

--- 2021/6.js
+++ 2021/6.js
@@ -4,7 +4,7 @@ function day6(input, part2) {
     let counts = Array(9).fill().map((_, i) => input.count(i))
 
     for (let i = 0; i < (part2 ? 256 : 80); i++) {
-        counts = counts.rotate(1)
+        counts.push(counts.shift())
         counts[6] += counts[8]
     }
 
