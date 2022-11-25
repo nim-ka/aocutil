d5fcd1c488e11ee83a6986cec1428f6b0b3d1b39
nim-ka <raumaank1@gmail.com>
2022-11-25 04:23:55 +0000

    .

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ if (typeof window == "undefined" && process.argv[2] == "test") {
 
 	const year = "2021"
 
-	for (let i = +process.argv[3] || 1; i <= 22; i++) {
+	for (let i = +process.argv[3] || 1; i <= 23; i++) {
 		const func = require(`./${year}/${i}.js`)
 		const input = fs.readFileSync(`./${year}/inputs/${i}`, "utf8")
 		const answers = fs.readFileSync(`./${year}/answers/${i}`, "utf8").split("\n-----\n")
