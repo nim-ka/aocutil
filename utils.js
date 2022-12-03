0a8759cb1dfb0838d5ac01876307bfae41d42153
nim-ka <raumaank1@gmail.com>
2022-12-03 00:51:17 +0000

    .

--- utils.js
+++ utils.js
@@ -1,5 +1,6 @@
 utils = {
-	log: (e, copy = false) => (console.log(copy ? e.copyDeep() : e), e),
+	log: (e, ...args) => (console.log(e, ...args), e),
+	logCopy: (e, ...args) => (console.log(e.copyDeep(), ...args), e),
 	fetch: (url) => fetch(url).then(e => e.text()),
 	fetchEval: (url) => utils.fetch(url).then(e => eval(e)),
 	signAgnosticInclusiveRange: (a, b, s = Math.sign(a - b)) => Array((a - b) * s + 1).fill().map((_, i) => a - i * s),
