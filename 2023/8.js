59c32c556331523061670644b060f484f1d8f7f8
nim-ka <alexing105@gmail.com>
2023-12-07 21:36:55 -0800

    day8

---
+++ 2023/8.js
@@ -0,0 +1,31 @@
+function day8(input, part2) {
+	let [steps, ...paths] = input.match(/[A-Z]+/g)
+	let cxns = {}
+	let nodes = []
+
+	for (let i = 0; i < paths.length; i += 3) {
+		if (part2 ? paths[i].endsWith("A") : paths[i] == "AAA") {
+			nodes.push(paths[i])
+		}
+
+		cxns[paths[i]] = [paths[i + 1], paths[i + 2]]
+	}
+
+	let count = 1
+
+	for (let node of nodes) {
+		let i = 0
+
+		while (!(part2 ? node.endsWith("Z") : node == "ZZZ")) {
+			node = cxns[node][+(steps[i++ % steps.length] == "R")]
+		}
+
+		count = count.lcm(i)
+	}
+
+	return count
+}
+
+if (typeof window == "undefined") {
+	module.exports = day8
+}
