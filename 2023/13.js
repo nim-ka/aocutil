bf854b6fe98e9fab3a21143c3a27d6529adfece0
nim-ka <alexing105@gmail.com>
2023-12-13 05:02:44 -0800

    i silly

--- 2023/13.js
+++ 2023/13.js
@@ -1,13 +1,12 @@
 function day13(input, part2) {
 	return input.split("\n\n").sum((str) => {
 		let grid = Grid.fromStr(str)
-		let r
-		let c
+		let idx
 
-		out: for (c = grid.width - 1; c > 0; c--) {
+		out: for (idx = grid.width - 1; idx > 0; idx--) {
 			let count = 0
 
-			for (let x = c - 1, nx = c; x >= 0 && nx < grid.width; x--, nx++) {
+			for (let x = idx - 1, nx = idx; x >= 0 && nx < grid.width; x--, nx++) {
 				for (let y = 0; y < grid.height; y++) {
 					if (grid.get(new Point(x, y)) != grid.get(new Point(nx, y))) {
 						if (++count > part2) {
@@ -22,10 +21,14 @@ function day13(input, part2) {
 			}
 		}
 
-		out: for (r = grid.height - 1; r > 0; r--) {
+		if (idx) {
+			return idx
+		}
+
+		out: for (idx = grid.height - 1; idx > 0; idx--) {
 			let count = 0
 
-			for (let y = r - 1, ny = r; y >= 0 && ny < grid.height; y--, ny++) {
+			for (let y = idx - 1, ny = idx; y >= 0 && ny < grid.height; y--, ny++) {
 				for (let x = 0; x < grid.width; x++) {
 					if (grid.get(new Point(x, y)) != grid.get(new Point(x, ny))) {
 						if (++count > part2) {
@@ -40,7 +43,7 @@ function day13(input, part2) {
 			}
 		}
 
-		return r * 100 + c
+		return idx * 100
 	})
 }
 
