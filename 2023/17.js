ccc0a89927cf87440cc6931a2083beae94a8ec26
nim-ka <alexing105@gmail.com>
2023-12-17 02:08:33 -0800

    this changes nothing

--- 2023/17.js
+++ 2023/17.js
@@ -6,9 +6,9 @@ function day17(input, part2) {
 	let minWalk = part2 ? 4 : 1
 	let maxWalk = part2 ? 10 : 3
 
-	let start1 = new Node(grid.topleft().encode() << 1 | 0)
-	let start2 = new Node(grid.topleft().encode() << 1 | 1)
-	let end = new Node(grid.bottomright().encode() << 1)
+	let start1 = new Node(0 << 16 | 0 << 8 | 0)
+	let start2 = new Node(0 << 16 | 0 << 8 | 1)
+	let end = new Node((grid.width - 1) << 16 | (grid.height - 1) << 8)
 
 	let nodes = {
 		[start1.val]: start1,
@@ -20,14 +20,15 @@ function day17(input, part2) {
 	return new Node().addCxn(start1, 0).addCxn(start2, 0).dijkstraTo(
 		end,
 		(node) => {
+			let x = node.val >> 16
+			let y = node.val >> 8 & 0xFF
 			let hor = node.val & 1
-			let pt = Point.decode(node.val >> 1)
 
 			for (let dir of [-1, 1]) {
 				for (let i = dir, dist = 0; -maxWalk <= i && i <= maxWalk; i += dir) {
 					let newPt = hor ?
-						new Point(pt.x, pt.y + i) :
-						new Point(pt.x + i, pt.y)
+						new Point(x, y + i) :
+						new Point(x + i, y)
 
 					if (!grid.contains(newPt)) {
 						break
@@ -36,7 +37,7 @@ function day17(input, part2) {
 					dist += grid.get(newPt)
 
 					if (i >= minWalk || i <= -minWalk) {
-						let state = newPt.encode() << 1 | !hor
+						let state = newPt.x << 16 | newPt.y << 8 | !hor
 						node.addCxn(nodes[state] ??= new Node(state), dist)
 					}
 				}
