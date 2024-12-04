f75d9520413f7e5f6edb66cfd9be74c54010f9da
nim-ka <alexing105@gmail.com>
2024-12-03 22:53:29 -0800

    day4 slower but more elegant

--- 2024/4.js
+++ 2024/4.js
@@ -2,39 +2,30 @@ function day4(input, part2) {
 	let grid = Grid.fromStr(input)
 	let count = 0
 	let dirs = part2 ? Point.ORIGIN.getUnfilteredDiagNeighbors() : Point.ORIGIN.getUnfilteredAllNeighbors()
+	let str = "XMAS"
+	let offset = part2 * 2
 
 	for (let x = 0; x < grid.width; x++) {
 		for (let y = 0; y < grid.width; y++) {
 			let pt = new Point(x, y)
-			if (grid.get(pt) != (part2 ? "A" : "X")) {
+			if (grid.get(pt) != str[offset]) {
 				continue
 			}
 
-			if (part2) {
-				let sat = 0
+			let sat = 0
 
-				for (let dir of dirs) {
-					if (grid.getDef(pt.add(dir)) == "M" && grid.getDef(pt.sub(dir)) == "S") {
-						sat++
-						if (sat == 2) {
-							break
-						}
+			out: for (let dir of dirs) {
+				for (let i = 1; i <= 3; i++) {
+					let head = pt.add(dir.mult(i - offset))
+					if (!grid.contains(head) || grid.get(head) != str[i]) {
+						continue out
 					}
 				}
 
-				count += sat == 2
-			} else {
-				out: for (let dir of dirs) {
-					for (let i = 1; i < "XMAS".length; i++) {
-						let head = pt.add(dir.mult(i))
-						if (!grid.contains(head) || grid.get(head) != "XMAS"[i]) {
-							continue out
-						}
-					}
-
-					count++
-				}
+				sat++
 			}
+
+			count += part2 ? sat == 2 : sat
 		}
 	}
 
