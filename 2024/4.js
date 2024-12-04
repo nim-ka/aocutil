ffc443f4f549d7a4746a6f8f6e6ef24fa2fdb6b2
nim-ka <alexing105@gmail.com>
2024-12-03 23:09:25 -0800

    day4 but slightly uglier but faster

--- 2024/4.js
+++ 2024/4.js
@@ -1,24 +1,30 @@
 function day4(input, part2) {
 	let grid = Grid.fromStr(input)
-	let count = 0
+
 	let dirs = part2 ? Point.ORIGIN.getUnfilteredDiagNeighbors() : Point.ORIGIN.getUnfilteredAllNeighbors()
+	let offset = part2 ? 2 : 0
+
 	let str = "XMAS"
-	let offset = part2 * 2
+	let center = str[offset]
+
+	let count = 0
 
 	for (let x = 0; x < grid.width; x++) {
 		for (let y = 0; y < grid.height; y++) {
 			let pt = new Point(x, y)
-			if (grid.get(pt) != str[offset]) {
+			if (grid.get(pt) != center) {
 				continue
 			}
 
 			let sat = 0
-
-			out: for (let dir of dirs) {
+			dirs_loop: for (let dir of dirs) {
 				for (let i = 1; i <= 3; i++) {
-					let head = pt.add(dir.mult(i - offset))
-					if (!grid.contains(head) || grid.get(head) != str[i]) {
-						continue out
+					let mult = i - offset
+					if (mult) {
+						let head = pt.add(dir.mult(mult))
+						if (!grid.contains(head) || grid.get(head) != str[i]) {
+							continue dirs_loop
+						}
 					}
 				}
 
