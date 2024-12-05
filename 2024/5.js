1efb704670aed97d3aba41c7a52b7eca7c1a62c9
nim-ka <alexing105@gmail.com>
2024-12-05 03:44:50 -0800

    xd

--- 2024/5.js
+++ 2024/5.js
@@ -1,17 +1,12 @@
 function day5(input, part2) {
 	let [ordering, lists] = input.split("\n").splitOn("")
-
-	ordering = ordering.dict((o, e) => {
-		let [left, right] = e.split("|")
-		o[left] ??= new Set()
-		o[left].add(right)
-	})
+	let rules = new Set(ordering.map((e) => (+e[0] * 10 + +e[1]) << 16 | (+e[3] * 10 + +e[4])))
 
 	let sum = 0
 
 	for (let line of lists) {
 		let unsorted = line.split(",")
-		let sorted = unsorted.slice().sort((a, b) => ordering[a].has(b) ? -1 : 1)
+		let sorted = unsorted.slice().sort((a, b) => rules.has(+a << 16 | +b) ? -1 : 1)
 
 		if (sorted.equals(unsorted) != part2) {
 			sum += +sorted[sorted.length >> 1]
