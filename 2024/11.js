6f08c2c568f31908873e3327cfebc0fb7cc331c9
nim-ka <alexing105@gmail.com>
2024-12-10 22:04:34 -0800

    day11 whoops

--- 2024/11.js
+++ 2024/11.js
@@ -1,23 +1,23 @@
-let count = utils.memoize((num, ticks) => {
-	if (ticks == 0) {
-		return 1
-	}
-
-	if (num == 0) {
-		return count(1, ticks - 1)
-	}
-
-	let str = num.toString()
-	if (str.length % 2 == 0) {
-		let left = +str.slice(0, str.length / 2)
-		let right = +str.slice(str.length / 2)
-		return count(left, ticks - 1) + count(right, ticks - 1)
-	}
-
-	return count(num * 2024, ticks - 1)
-})
-
 function day11(input, part2) {
+	let count = utils.memoize((num, ticks) => {
+		if (ticks == 0) {
+			return 1
+		}
+
+		if (num == 0) {
+			return count(1, ticks - 1)
+		}
+
+		let str = num.toString()
+		if (str.length % 2 == 0) {
+			let left = +str.slice(0, str.length / 2)
+			let right = +str.slice(str.length / 2)
+			return count(left, ticks - 1) + count(right, ticks - 1)
+		}
+
+		return count(num * 2024, ticks - 1)
+	}, (num, ticks) => num * 256 + ticks)
+
 	return input.posints().sum((num) => count(num, part2 ? 75 : 25))
 }
 
